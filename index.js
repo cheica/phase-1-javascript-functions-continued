017b33b9d01ae6f28c7081543f797ab18022703e
Chelsea <chelsea.icaza10@hotmail.com>
2021-07-20 17:22:45 -0400

    commit tree 165f49382b9daa1b503a4795a0ab8b107c5ed216

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+function saturdayFun (activity = 'roller-skate'){
+    
+    return 'This Saturday, I want to ' + `${activity}` + "!"
+}
+
+function mondayWork(activity2= 'go to the office'){
+
+    return 'This Monday, I will ' + `${activity2}` + "."
+}
+
+function wrapAdjective(result = '*'){
+    return function boldHighlight(wrapAdjective = 'special'){
+        return 'You are ' + `${result}` + wrapAdjective + `${result}` + '!'
+        
+    }
+    } 
